b158b9c6955735214ad088db01310cb480cf34ea
pengfy <13531693134@163.com>
2019-08-29 11:56:53 +0800

    在radio中进行文字识别，并作为判断依据

--- src/get_img_ROI.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from PIL import Image
-img = Image.open("file.png")
-print(img.size)
-cropped = img.crop((800, 325, 1280, 490))  # (left, upper, right, lower)
-cropped.save("file1.png")
--- utils/img2str.py
+++ utils/img2str.py
@@ -3,6 +3,14 @@ import pytesseract
 from PIL import Image
 import settings.DIR_PATH as path
 
-pytesseract.pytesseract.tesseract_cmd = 'D://Program Files (x86)/Tesseract-OCR/tesseract.exe'
-img = Image.open(path.src_path+'file1.png')
-print(pytesseract.image_to_string(img,lang='eng'))
+def get_str():
+    pytesseract.pytesseract.tesseract_cmd = 'D://Program Files (x86)/Tesseract-OCR/tesseract.exe'
+    img = Image.open(path.src_path+'file1.png')
+    text = pytesseract.image_to_string(img,lang='eng')
+    try:
+        return text.split()[0]
+    except:
+        return "0"
+
+if __name__ == "__main__":
+    print(get_str())
