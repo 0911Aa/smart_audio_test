0ccfde59dba2042fbb396def749e2f5d0a2e21d6
0911Aa <52586793+0911Aa@users.noreply.github.com>
2019-11-14 16:47:59 +0800

    Update check_time_and_result.py

--- utils/check_time_and_result.py
+++ utils/check_time_and_result.py
@@ -13,9 +13,9 @@ def check(check_time,result,case_name):
         print("\033[7;31m%scase pass...\033[0m"%case_name)
     else:
         fail_dir_name = time.strftime("%d_%H_%M_%S")
-		dir_path = path.DIR_PATH + "\\report\\error_log\\" + fail_dir_name + case_name+"\\"
-		print(dir_path)
-		#os.makedirs(dir_path)
+        dir_path = path.DIR_PATH + "\\report\\error_log\\" + fail_dir_name + case_name+"\\"
+        print(dir_path)
+        os.makedirs(dir_path)
         cmd = "adb pull sdcard/txz/log " + dir_path
         print(cmd)
         os.system(cmd)
