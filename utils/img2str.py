fea302aaf722d359fea44084cce36d458c09603e
0911Aa <52586793+0911Aa@users.noreply.github.com>
2019-11-14 18:59:03 +0800

    Update img2str.py

--- utils/img2str.py
+++ utils/img2str.py
@@ -4,7 +4,7 @@ from PIL import Image
 import settings.DIR_PATH as path
 
 def get_str():
-    pytesseract.pytesseract.tesseract_cmd = 'D://Program Files (x86)/Tesseract-OCR/tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = 'C://Program Files/Tesseract-OCR/tesseract.exe'
     img = Image.open(path.src_path+'file1.png')
     text = pytesseract.image_to_string(img,lang='eng')
     try:
