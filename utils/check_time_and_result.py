00dc2dcedf70ddf248719c230881c4da985ce81f
0911Aa <52586793+0911Aa@users.noreply.github.com>
2019-11-14 15:55:19 +0800

    result update

--- utils/check_time_and_result.py
+++ utils/check_time_and_result.py
@@ -10,10 +10,12 @@ def check(check_time,result,case_name):
     else:
         print("\033[7;31m%s唤醒成功\033[0m"%case_name)
     if result and status:
-        print("\033[7;31m%s测试通过\033[0m"%case_name)
+        print("\033[7;31m%scase pass...\033[0m"%case_name)
     else:
         fail_dir_name = time.strftime("%d_%H_%M_%S")
-        cmd = "adb pull sdcard/txz/log " + path.DIR_PATH + "\\report\\error_log\\" + fail_dir_name + case_name+"\\"
+		dir_path = path.DIR_PATH + "\\report\\error_log\\" + fail_dir_name + case_name+"\\"
+		os.makedirs(dir_path)
+        cmd = "adb pull sdcard/txz/log " + dir_path
         print(cmd)
         os.system(cmd)
         raise Exception('测试失败')
